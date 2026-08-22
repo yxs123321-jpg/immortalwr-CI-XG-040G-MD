a1f4d104fe4de3dec20803613c87da8cd4837846
yxs123321-jpg <yxs123321@gmail.com>
2026-08-22 09:11:55 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -82,6 +82,8 @@ UPDATE_PACKAGE "viking" "VIKINGYFY/packages" "main" "" "gecoosac luci-app-timewo
 UPDATE_PACKAGE "luci-app-airoha-npu" "bingoguo93/luci-app-airoha-npu" "main"
 UPDATE_PACKAGE "luci-app-lucky" "sirpdboy/luci-app-lucky" "main"
 UPDATE_PACKAGE "luci-app-tailscale-community" "Tokisaki-Galaxy/luci-app-tailscale-community" "master" "" "luci-app-tailscale-community"
+UPDATE_PACKAGE "luci-app-dae" "sbwml/luci-app-dae" "main"
+UPDATE_PACKAGE "v2ray-geodata" "sbwml/v2ray-geodata" "main"
 
 #更新软件包版本
 UPDATE_VERSION() {
