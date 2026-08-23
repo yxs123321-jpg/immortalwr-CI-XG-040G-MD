154e05e4fc323830b0bb63431a6f5f50b27b43fa
yxs123321-jpg <yxs123321@gmail.com>
2026-08-24 05:46:59 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -87,6 +87,7 @@ UPDATE_PACKAGE "v2ray-geodata" "sbwml/v2ray-geodata" "master"
 UPDATE_PACKAGE "luci-app-run" "wukongdaily/luci-app-run" "master"
 UPDATE_PACKAGE "istore" "linkease/istore" "main"
 UPDATE_PACKAGE "openwrt-daede" "kenzok8/openwrt-daede" "main"
+UPDATE_PACKAGE "luci-app-minigate" "tpxcer/luci-app-minigate" "main"
 
 #更新软件包版本
 UPDATE_VERSION() {
