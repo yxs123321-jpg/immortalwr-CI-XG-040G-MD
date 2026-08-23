a5768812eb619d3c388b886d0f121fc471edfd2c
yxs123321-jpg <yxs123321@gmail.com>
2026-08-23 23:12:17 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -86,7 +86,6 @@ UPDATE_PACKAGE "luci-app-dae" "sbwml/luci-app-dae" "main"
 UPDATE_PACKAGE "v2ray-geodata" "sbwml/v2ray-geodata" "master"
 UPDATE_PACKAGE "luci-app-run" "wukongdaily/luci-app-run" "master"
 UPDATE_PACKAGE "istore" "linkease/istore" "main"
-UPDATE_PACKAGE "luci-app-istorex" "linkease/luci-app-istorex" "main"
 UPDATE_PACKAGE "openwrt-daede" "kenzok8/openwrt-daede" "main"
 
 #更新软件包版本
