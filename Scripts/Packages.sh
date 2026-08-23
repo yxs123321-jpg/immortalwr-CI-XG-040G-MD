a59720c6cefbab31bcf3511b3ff86ecc15406950
yxs123321-jpg <yxs123321@gmail.com>
2026-08-23 22:15:01 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -84,7 +84,7 @@ UPDATE_PACKAGE "luci-app-lucky" "sirpdboy/luci-app-lucky" "main"
 UPDATE_PACKAGE "luci-app-tailscale-community" "Tokisaki-Galaxy/luci-app-tailscale-community" "master" "" "luci-app-tailscale-community"
 UPDATE_PACKAGE "luci-app-dae" "sbwml/luci-app-dae" "main"
 UPDATE_PACKAGE "v2ray-geodata" "sbwml/v2ray-geodata" "master"
-UPDATE_PACKAGE "luci-app-run" "wukongdaily/luci-app-run" "main"
+UPDATE_PACKAGE "luci-app-run" "wukongdaily/luci-app-run" "master"
 UPDATE_PACKAGE "istore" "linkease/istore" "main"
 
 #更新软件包版本
